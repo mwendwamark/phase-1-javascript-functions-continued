9e07be2672d48dc022d18929a06900089943931f
mark mwendwa <mwendwamark310@gmail.com>
2023-03-18 15:17:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+   return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(task = "go to the office") {
+
+   return `This Monday, I will ${task}.`;
+
+}
+
+function outer(greeting, msg = "It's a fine day to learn") {
+   
+   const innerFunction = function (name, lang = "Python") {
+      
+      return `${greeting}, ${name}! ${msg} ${lang}`; 
+   };
+
+   return innerFunction("student", "JavaScript"); 
+}
+
+
+function wrapAdjective(symbol = "*") {
+
+   return function inner(param = "special") {
+
+      return `You are ${symbol}${param}${symbol}!`;
+
+   };
+}
+
